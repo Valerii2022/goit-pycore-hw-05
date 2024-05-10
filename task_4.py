1ab0a1945b132bde318b63f0cf4dfc7d4d835113
Valerii Pometun <vmpometun@gmail.com>
2024-05-10 12:58:15 +0300

    added decorators for task#4

--- task_4.py
+++ task_4.py
@@ -1,45 +1,52 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found. Please provide a valid name."
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Please provide the correct number of arguments."
+        except Exception:
+            return "An unexpected error occurred. Please try again."
+    return inner
+
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, contacts):
-    try:
-        name, phone = args
+    name, phone = args
+    if name in contacts:
+        return f"Contact {name} already exists"
+    else:
         contacts[name] = phone
         return "Contact added."
-    except ValueError:
-        return "Not enough values to unpack"
 
+@input_error
 def change_contact(args, contacts):
-    try:
-        name, new_phone = args
-        if name in contacts:
-            contacts[name] = new_phone
-            return f"Contact {name} changed."
-        else:
-            return f"Contact {name} not found"
-    except ValueError:
-        return "Not enough values to unpack"
+    name, new_phone = args
+    if name in contacts:
+        contacts[name] = new_phone
+        return f"Contact {name} changed."
+    else:
+        return f"Contact {name} not found"
 
+@input_error
 def show_phone(args, contacts):
-    try:
-        name = args[0]
-        if name in contacts:
-            return f"The phone number for '{name}' is {contacts[name]}."
-        else:
-            return f"Contact {name} not found"
-    except Exception:
-        return "Error.Please try again!" 
+    name = args[0]
+    return f"The phone number for '{name}' is {contacts[name]}."
 
+@input_error
 def show_all(contacts):
-    try:
-        if len(contacts) == 0:
-            return "You have no contacts yet."   
-        else:
-            return contacts
-    except Exception:
-        return "Error.Please try again!"
+    if len(contacts) == 0:
+        return "You have no contacts yet."   
+    else:
+        return contacts
 
 def main():
     contacts = {}
